858a5eac152475d184789797b0f0ba9cb060a5e8
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-11-04 19:25:05 +0000

    Added sample Application

---
+++ branches/alpha/applications/TestApplication/TestApplication_Controller.php
@@ -0,0 +1,44 @@
+<?php
+
+// *Always*, your Controller must:
+// 1) extend "leaf_Controller", and
+// 2) have a name ending the string "_Controller".
+//
+// If those two requirements, are not met,
+// your Controller will be ignored, and an error
+// will be raised.
+//
+// Also, you *must*, call the parent`s contructor,
+// in order to have your Controller working properly.
+class TestApplication_Controller extends leaf_Controller {
+
+
+	public function __construct()
+	{
+        // Call the parent`s contructor.
+		parent::__construct();
+
+        // Load SampleModel and bind it under
+        // the name 'sample'.
+        $this->loader->model('SampleModel', array('Bind'=>'sample'));
+	}
+	
+	public function index()
+	{
+        // Print a message.
+        echo "Success! This is a test controller.<br/>\n";
+
+        // Change on the fly, the configuration, so 
+        // no debug statistics are created.
+        $this->config['enable_debug_stats'] = "No";
+
+        // Retrieve from the query string the variable 'name'.
+        // Then, print the contents (if any).
+        $testQString = $this->request->getQueryString('name');
+
+        if (!empty($testQString))
+            echo $testQString;
+	}
+}
+
+?>
