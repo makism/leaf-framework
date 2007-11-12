bc8f2d3cd65d72b06f5ccad48856c1df1f36b7a8
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-11-12 08:00:27 +0000

    20071112 - Pre-alpha

---
+++ tags/pre-alpha/applications/TestApplication/Model/SampleModel.php
@@ -0,0 +1,18 @@
+<?php
+
+class SampleModel_Model extends leaf_Model {
+
+	public function __construct()
+	{
+        /** _Always_ call the parent constructor */
+		parent::__construct();
+	}
+
+    public function doSomethingFancy()
+    {
+        // doing something fancy...
+    }
+
+}
+
+?>
---
+++ tags/pre-alpha/leaf/core/hook/Hook.php
@@ -0,0 +1,50 @@
+﻿<?php
+/**
+ * This source file is part of the leaf framework and
+ * is licensed under the New BSD license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     http://leaf-framework.sourceforge.net/LICENSE/  New BSD License
+ * @link        http://leaf-framework.sourceforge.net
+ */
+
+
+/**
+ *
+ * @package		leaf
+ * @subpackage	core.hook
+ * @author		Avraam Marimpis <makism@users.sf.net>
+ * @version		$Id$
+ * @todo
+ * <ol>
+ *  <li>Implement.</li>
+ * </ol>
+ */
+abstract class leaf_Hook extends leaf_Loader {
+
+    const LEAF_REG_KEY = "hook";
+    
+    const LEAF_CLASS_ID = "LEAF_HOOK-1_0_dev";
+    
+
+	/**
+     *
+     *
+     * @return  void
+     */
+	public function __construct()
+	{
+        parent::__construct();
+	}
+
+    /**
+     *
+     *
+     * @return  string
+     */
+    abstract public function __toString();
+
+}
+
+?>
