649d44889c4de043fb8f5986d658bb0a9ed255db
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-11-18 19:56:26 +0000

    Added support for loading plugins

--- trunk/leaf/core/Loader.php
+++ trunk/leaf/core/Loader.php
@@ -10,7 +10,9 @@
 
 
 /**
- *
+ * Handles the loading requests for resources like the Models,
+ * Extensions, Plugins, Libraries etc.
+ * 
  * @package		leaf
  * @subpackage	core
  * @author		Avraam Marimpis <makism@users.sf.net>
@@ -18,14 +20,32 @@
  * @todo
  * <ol>
  *  <li>Implement.</li>
+ *  <li>Document.</li>
  * </ol>
  */
 class leaf_Loader extends leaf_Base {
 
-    const LEAF_REG_KEY = "loader";
+    const LEAF_REG_KEY = "load";
     
     const LEAF_CLASS_ID = "LEAF_LOADER-1_0_dev";
 
+    /**
+     * List of all the loaded plugins
+     * 
+     * @var array 
+     */
+    private $plugins = array();
+    
+    /*
+     * List of "black-listed" plugins.
+     * 
+     * These plugins are found to have some problems
+     * and cannot be loaded.
+     *  
+     * @var array
+     */
+    private $pluginsBlacklist = array();
+    
 
     /**
      *
@@ -37,12 +57,6 @@ class leaf_Loader extends leaf_Base {
         parent::__construct(self::LEAF_REG_KEY);
     }
 
-
-    public function __toString()
-    {
-        return __CLASS__ . " " . self::LEAF_CLASS_ID;
-    }
-
     /**
      *
      *
@@ -50,7 +64,7 @@ class leaf_Loader extends leaf_Base {
      * @param   array   $opts
      * @return  void
      */
-    public function model($modelName, $opts=NULL)
+    public function model($modelName, array $opts=NULL)
     {
     }
 
@@ -89,15 +103,36 @@ class leaf_Loader extends leaf_Base {
     }
 	
     /**
-     *
+     * Loads a "plugin".
+     * 
+     * "Plugin", is a common php script file that contains
+     * a number of functions.<br>
+     * It is most likely, that each plugin, will have a specific
+     * "topic".
+     * 
      * @param   string  $plugin
      * @return  void
      */
 	public function plugin($plugin)
 	{
-	
+		if (!in_array($plugin, $this->plugins)) {
+			//"applications/" . $app . "/View/" . $name . ".php";
+			$fileName = "leaf/plugins/" . $plugin . ".php";
+			if (file_exists($fileName) && is_readable($fileName)) {
+				require_once $fileName;
+				
+				$this->plugins[] = $plugin;
+			} else {
+				// add to "blacklist"
+				$this->pluginsBlacklist[] = $plugin;
+			}
+		}
 	}
 
+    public function __toString()
+    {
+        return __CLASS__ . " " . self::LEAF_CLASS_ID;
+    }
 }
 
 ?>
