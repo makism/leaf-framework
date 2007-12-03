52bdd407fad7fc1cb3a7d54037131c7a87f8dc0d
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-03 07:58:04 +0000

    Introducing the 'Db' library.Handles the 'database profiles' and their references as well as setting session or global connection properties.

--- branches/makism-bleeding-edge-upstream-dev/leaf/core/db/Db.php
+++ branches/makism-bleeding-edge-upstream-dev/leaf/core/db/Db.php
@@ -11,6 +11,37 @@
 
 /**
  *
+ *
+ */
+//require_once LEAF_BASE . "core/db/Db_Backend.php";
+
+/**
+ *
+ *
+ */
+//require_once LEAF_BASE . "core/db/Db_ActiveRecord.php";
+
+/**
+ *
+ *
+ */
+//require_once LEAF_BASE . "core/db/Db_Factory.php";
+
+/**
+ *
+ * 
+ */
+define('DB_SCOPE_PROFILE', 1);
+
+/**
+ * 
+ * 
+ */
+define('DB_SCOPE_GLOBAL', 2);
+
+
+/**
+ *
  * @package     leaf
  * @subpackage  core.db
  * @author      Avraam Marimpis <makism@users.sf.net>
@@ -24,21 +55,109 @@ final class leaf_Db extends leaf_Base {
 
     const LEAF_REG_KEY = "Db";
     
-    const LEAF_CLASS_ID = "LEAF_DB-1_0_dev";
+    const LEAF_CLASS_ID = "LEAF_DB-\$Revision\$";
+
+    
+    /**
+     * An array holding all the different database connections. 
+     * 
+     * @var array
+     */
+    private $connectionPool = array();
+    
+    /**
+     * Currently supported database backends.
+     * 
+     * For the time being, we support the classic MySQL extension.
+     * "mysqli", "sqlite2" and "pdo" will follow.
+     * 
+     * @var array
+     */
+    private $supportedBackends = array("mysql");
     
     
     /**
      * 
      * 
+     * @return  void
      */
     public function __construct()
     {
         parent::__construct(self::LEAF_REG_KEY);
+
+    }
+    
+    /**
+     * 
+     * 
+     * The parameter "scope" specifies the scope in which
+     * the defined options will be visible.<br>
+     * For example you may specify (<i>in Controller scope</i>):
+     * <code>$this->Db->set("encoding", "utf8", DB_SCOPE_PROFILE);</code>
+     * this will result in making the options only valid for the
+     * current profile you have already bound to.<br>
+     * Keep in mind that some of the options have a default scope which
+     * cannot be changed.
+     * 
+     * @param   string  $parameter
+     * @param   string  $value
+     * @param   integer $scope
+     * @return  void
+     */
+    public function set($parameter, $value, $scope=0)
+    {
+        
+    }
+    
+    /**
+     * 
+     * 
+     * @param   string  $profileName
+     * @param   array   $options
+     * @return  void
+     */
+    public function bind($profileName, array $options=NULL)
+    {
+        
+    }
+    
+    /**
+     * Returns a connection...
+     *
+     * @return  mixed
+     */
+    public function __get($offset)
+    {
+        
+    }
+    
+    /**
+     * 
+     * 
+     * @param   string  $method
+     * @param   array   $arguments
+     * @return  mixed
+     */
+    public function __call($method, $arguments)
+    {
+        
+    }
+    
+    /**
+     * 
+     * 
+     * @return void
+     */
+    private function factoryBackend($target)
+    {
+        if (in_array($this->supportedBackends)) {
+                        
+        }
     }
     
     public function __toString()
     {
-        return __CLASS__ . " " . self::LEAF_CLASS_ID;
+        return __CLASS__ . " (Adds support for communicating with databases)";
     }
     
 }
