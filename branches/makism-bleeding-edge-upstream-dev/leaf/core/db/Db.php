a2783970e29f94095850b1958c4e67b03e3c9862
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-03 20:47:42 +0000

    Added support for retrieving a link from the connection pool, using the magic method __get. Added method for "binding" a db profile (from the conf file), and thus handling a connection. Modified the class constructor to discover the built-in extensions itself. Added support for passing arguments to the class constructor, thus enabling options like auto-connect, etc.

--- branches/makism-bleeding-edge-upstream-dev/leaf/core/db/Db.php
+++ branches/makism-bleeding-edge-upstream-dev/leaf/core/db/Db.php
@@ -10,31 +10,27 @@
 
 
 /**
- *
- *
+ * 
  */
-//require_once LEAF_BASE . "core/db/Db_Backend.php";
+require_once LEAF_BASE . "core/db/Db_Backend.php";
 
 /**
- *
- *
+ * 
  */
-//require_once LEAF_BASE . "core/db/Db_ActiveRecord.php";
+require_once LEAF_BASE . "core/db/Db_ActiveRecord.php";
+
 
 /**
- *
- *
+ * 
  */
-//require_once LEAF_BASE . "core/db/Db_Factory.php";
+require_once LEAF_BASE . "core/db/backend/mysql.php";
 
 /**
- *
  * 
  */
 define('DB_SCOPE_PROFILE', 1);
 
 /**
- * 
  * 
  */
 define('DB_SCOPE_GLOBAL', 2);
@@ -69,22 +65,69 @@ final class leaf_Db extends leaf_Base {
      * Currently supported database backends.
      * 
      * For the time being, we support the classic MySQL extension.
-     * "mysqli", "sqlite2" and "pdo" will follow.
+     * "mysqli", "sqlite2", "pdo" and "oracle" will follow.
      * 
      * @var array
      */
-    private $supportedBackends = array("mysql");
+    private $supportedBackends = array();
+    
+    /**
+     * Holds all the declared profiles in the database
+     * configuration file.
+     * 
+     * @var array
+     */
+    private $allProfiles = array();
+    
+    /**
+     * All the parameters specified in the database configuration file.
+     * 
+     * @var array
+     */
+    private $options = array();
     
     
     /**
      * 
      * 
+     * @param   array   $opts
      * @return  void
+     * @todo
+     * <ol>
+     *  <li>Check if the assignment of the db conf option can be ignored.
+     *  This will be replaced, with a reference to the actual options using
+     *  the Config object via the Registry.</li>
+     * </ol>
      */
-    public function __construct()
+    public function __construct(array $opts=NULL)
     {
         parent::__construct(self::LEAF_REG_KEY);
+        
+        // Currently, we support only the classic "mysql" extensions.
+        // Let`s find out if they are built-in.
+        if (extension_loaded("mysql"))
+            $this->supportedBackends[] = "mysql";
+        
+        // Fetch all the database configuration options. 
+        $confOpts = $this->Config->getByHashKey("database"); 
+        $this->allProfiles  = $confOpts['profiles'];
+        $this->options = $confOpts['general'];
+        
+        // Support for settings
+        if ($opts!=NULL) {
 
+            // Auto-bind the requested db profile
+            if (isset($opts['profile'])) {
+                $this->bind($opts['profile']);
+            }
+
+            // Auto-connect
+            if (isset($opts['auto_connect']) &&
+                $opts['auto_connect']==TRUE)
+            {
+                $this->{$opts['profile']}->connect();            
+            }
+        }
     }
     
     /**
@@ -103,10 +146,14 @@ final class leaf_Db extends leaf_Base {
      * @param   string  $value
      * @param   integer $scope
      * @return  void
+     * @todo 
+     * <ol>
+     *  <li>Implement.</li>
+     * </ol>
      */
     public function set($parameter, $value, $scope=0)
     {
-        
+        return;
     }
     
     /**
@@ -118,41 +165,43 @@ final class leaf_Db extends leaf_Base {
      */
     public function bind($profileName, array $options=NULL)
     {
-        
-    }
-    
-    /**
-     * Returns a connection...
-     *
-     * @return  mixed
-     */
-    public function __get($offset)
-    {
-        
-    }
-    
-    /**
-     * 
-     * 
-     * @param   string  $method
-     * @param   array   $arguments
-     * @return  mixed
-     */
-    public function __call($method, $arguments)
-    {
-        
-    }
-    
-    /**
-     * 
-     * 
-     * @return void
-     */
-    private function factoryBackend($target)
-    {
-        if (in_array($this->supportedBackends)) {
-                        
+        if (array_key_exists($profileName, $this->allProfiles)) {
+            
+            $curr = $this->allProfiles[$profileName];
+            
+            if (in_array($curr['backend'], $this->supportedBackends)) {
+                $driver = "leaf_Db_Backend_" . $curr['backend'];
+                $this->connectionPool[$profileName] = new $driver($curr);
+            }
         }
+    }
+    
+    /**
+     * Returns the requested object from the {@link leaf_Registry $Registry},
+     * <b>or</b> the requested database connection link.
+     * 
+     * @param  string  $key
+     * @return object|NULL
+     * @todo
+     * <ol>
+     *  <li>Support for accessing the Models using a member property named "Models".
+     *  <br>For example (<i>in Controller scope</i>):
+     *  <code>$this->Models->modelName</code> <b>or</b>
+     *  <code>$this->Models['modelName']</code><br>
+     *  This denotes changes in the {@link leaf_Loader} class as well.</li>
+     * </ol>
+     */
+    protected function __get($key)
+    {
+        // Search the requested key in the Registry and
+        // return it if found.
+        if (leaf_Registry::getInstance()->isRegistered($key)!=FALSE)
+            return parent::__get($key);
+            
+        // Otherwise, asume that a database connection link is
+        // requested and thus return it -if found-.
+        else
+            return $this->connectionPool[$key];
     }
     
     public function __toString()
