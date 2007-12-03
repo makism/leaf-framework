4ba1936bfb73d504e5719e31156d58f95bf07324
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-03 20:40:49 +0000

    Initial implementation of the mysql driver.

--- branches/makism-bleeding-edge-upstream-dev/leaf/core/db/backend/mysql.php
+++ branches/makism-bleeding-edge-upstream-dev/leaf/core/db/backend/mysql.php
@@ -23,15 +23,106 @@
  * </ol>
  */
 final class leaf_Db_Backend_mysql extends leaf_Db_Backend {
-    
-    
+   
 
-    public function __construct()
+    /**
+     *
+     * 
+     * @param   array   $profile
+     * @return  void
+     */
+    public function __construct(array $profile)
     {
         parent::__construct();
+        
+        $this->dbProfile = $profile;
     }
     
+    /**
+     * 
+     * 
+     * @return  boolean
+     */
+    public function connect()
+    {
+        $host = $this->dbProfile['hostname'];
+        $port = ($this->dbProfile['port']=="")
+                ? 3306
+                : $this->dbProfile['port'];
+        $user = $this->dbProfile['username'];
+        $pass = $this->dbProfile['password'];
+        $db   = $this->dbProfile['db_name'];
+        
+        $server = $host . ":" . $port;
+
+        $this->link = mysql_connect($server, $user, $pass);
+        
+        if ($db!="") {
+            $this->selectDb($db);
+        }
+        
+        if ($this->link!=NULL)
+            return true;
+        else
+            return false;
+    }
     
+    /**
+     * 
+     *
+     * @return  boolean
+     */
+    public function disconnect()
+    {
+        return;
+    }
+    
+    /**
+     * 
+     *
+     * @param   string  $dbName
+     * @return  boolean
+     */
+    public function selectDb($dbName)
+    {
+        return mysql_select_db($dbName, $this->link);
+    }
+    
+    /**
+     * 
+     * 
+     * @return  integer
+     */
+    public function connectionStatus()
+    {
+        
+    }
+    
+    /**
+     * 
+     * 
+     */
+    public function select($selectQuery)
+    {
+
+    }
+    
+    /**
+     * 
+     */
+    public function insert($insertQuery)
+    {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function query($rawQuery)
+    {
+        
+    }
+
 }
 
 ?>
