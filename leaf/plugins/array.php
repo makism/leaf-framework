6594154468f721ccf165cb2c972328cfce837e2f
Makis Marimpis <makism@users.noreply.github.com>
2007-12-17 09:39:25 +0000

    Introducing array-related plugins (unimplmented).

---
+++ leaf/plugins/array.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This source file is licensed under the New BSD license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     http://leaf-framework.sourceforge.net/licence/  New BSD License
+ * @link        http://leaf-framework.sourceforge.net
+ *
+ * @package     leaf
+ * @subpackage  plugins
+ * @author      Marimpis Avraam <makism@users.sf.net>
+ * @version     $Id$
+ * @filesource
+ */
+
+
+/**
+ * Converts an associative array (or all rows - in it`s size is greater than 1)
+ * to a class.
+ *  
+ * @param   array   $arr
+ * @return  object stClass
+ */
+function array2object() {
+
+}
+
+?>
