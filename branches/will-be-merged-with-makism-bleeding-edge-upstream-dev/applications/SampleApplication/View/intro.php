b83870c29fcf52f839d89a895bbf31e6c775d093
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-22 08:31:34 +0000

    Added "stats" link, using new plugins' functions. Modified CSS.

--- branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/applications/SampleApplication/View/intro.php
+++ branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/applications/SampleApplication/View/intro.php
@@ -1,7 +1,13 @@
-
-<div style="background-color: #ddffdd; border: 1px solid #bbffbb; padding: 4px; width: 520px;">
+﻿
+<div style="background-color: #ddffdd; border-top: 3px solid #bbffbb; margin: auto; padding: 4px; width: 520px;">
     <span style="font-weight: bold; font-size: 20px;">Welcome to the leaf framework!</span><br />
     <span style="font-style: italic;"><?php echo $desc; ?></span>
+	<br /><br />
+	<span style="font-size: 10px;">
+		<?php
+			echo make_link("stats", "Produce debug statistics");
+		?>
+	</span>
 </div>
 
 <br />
