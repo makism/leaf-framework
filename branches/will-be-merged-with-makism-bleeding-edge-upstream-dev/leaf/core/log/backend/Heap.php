b3741e9855aceb2bf0f0eb8fe0e8ef06a1e74e29
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-25 18:50:21 +0000

    Major rewrite.

--- branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/backend/Heap.php
+++ branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/backend/Heap.php
@@ -10,7 +10,8 @@
 
 
 /**
- *
+ * The logging is done using an internal buffer.
+ * The buffer is implemented by an array.
  *
  * @package     leaf
  * @subpackage  core.log.backend
@@ -20,21 +21,73 @@
 class leaf_Logger_Heap extends leaf_Log {
 
 	/**
-	 *
+	 * A stack with all the logged messages.
 	 *
 	 * @var	array
 	 */
 	private $stack = array();
-
+	
+	/**
+	 *
+	 *
+	 * @return	void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	/**
+	 * 
+	 *
+	 * @return void
+	 */
+	public function saveLog()
+	{
+		
+		/*$this->logLevel = $pack['level'];
+		$this->logMessage = $pack['message'];
+		$this->logFilename = (isset($pack['file'])) ? $pack['file'] : NULL;
+		$this->logClassname = (isset($pack['class'])) ?$pack['class'] : NULL;
+		$this->logException = (isset($pack['exception'])) ? $pack['exception'] : NULL;
+		$this->logStamp = time();*/
+		
+		$this->clearPackLog();
+	}
 	
 	/**
 	 *
 	 *
 	 *
 	 */
-	public function __construct()
+	public function getBuffer()
 	{
+		return;
+	}
 	
+	/**
+	 * 
+	 *
+	 * @return boolean
+	 */
+	public function supportsFlush()
+	{
+		return false;
+	}
+	
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function supportsBuffering()
+	{
+		return false;
+	}
+	
+	public function __toString()
+	{
+		return "Heap Backend";
 	}
 
 }
