2a6eb803d3cc56a5d1575a4f1926848513589706
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-25 18:47:34 +0000

    Major rewrite.

--- branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/Log.php
+++ branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/Log.php
@@ -16,16 +16,128 @@
  * @subpackage  core.log
  * @author		Avraam Marimpis <makism@users.sf.net>
  * @version	    $Id$
- * @method		void log()
- *					 log(string message)
- *					 log(Exception e)
- *					 log(int level, string message)
- *					 log(int level, string message, string fileName)
- *					 log(int level, string message, string fileName, string className)
- *					 log(int level, Exception e)
- *				Handles to log a message.
  */
-abstract class leaf_Log {
+abstract class leaf_Log  {
+
+	/**
+	 *
+	 *
+	 * @var	integer
+	 */
+	protected $logLevel = NULL;
+	
+	/**
+	 *
+	 *
+	 * @var	string
+	 */
+	protected $logMessage = NULL;
+	
+	/**
+	 *
+	 *
+	 * @var	string
+	 */
+	protected $logFilename = NULL;
+	
+	/**
+	 *
+	 *
+	 * @var	string
+	 */
+	protected $logClassname = NULL;
+	
+	/**
+	 *
+	 *
+	 * @var object Exception
+	 */
+	protected $logException = NULL;
+	
+	/**
+	 *
+	 *
+	 * @var integer
+	 */
+	protected $logStamp =NULL;
+	
+	
+	/**
+	 *
+	 *
+	 *
+	 * @return	void
+	 */
+	public function __construct()
+	{
+	
+	}
+	
+	/**
+	 *
+	 *
+	 * @param	array	$pack
+	 * @return	void
+	 */
+	public function setPackLog(array $pack)
+	{
+		$this->logLevel = $pack['level'];
+		$this->logMessage = $pack['message'];
+		$this->logFilename = (isset($pack['file'])) ? $pack['file'] : NULL;
+		$this->logClassname = (isset($pack['class'])) ?$pack['class'] : NULL;
+		$this->logException = (isset($pack['exception'])) ? $pack['exception'] : NULL;
+		$this->logStamp = time();
+	}
+	
+	/**
+	 * Clears the 
+	 *
+	 * @return	void
+	 */
+	protected function clearPackLog()
+	{
+		$this->logLevel = NULL;
+		$this->logMessage = NULL;
+		$this->logFilename = NULL;
+		$this->logClassname = NULL;
+		$this->logException = NULL;
+		$this->logStamp =NULL;
+	}
+	
+	/**
+	 *
+	 *
+	 * @return	array
+	 */
+	public abstract function getBuffer();
+	
+	/**
+	 *
+	 *
+	 * @return	boolean
+	 */
+	public abstract function supportsFlush();
+	
+	/**
+	 *
+	 *
+	 * @return	boolean
+	 */
+	public abstract function supportsBuffering();
+	
+	/**
+	 * Dumps the last log
+	 *
+	 * @return void
+	 */
+	public abstract function saveLog();
+	
+	/**
+	 *
+	 *
+	 * @return	string
+	 */
+	public abstract function __toString();
 
 }
 
