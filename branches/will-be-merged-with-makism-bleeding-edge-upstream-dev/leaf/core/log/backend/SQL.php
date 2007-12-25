4e4c759d22bfbbe2fd66eb50efbb2c85c12a0fb1
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2007-12-25 18:49:39 +0000

    Added page-level docblock.

--- branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/backend/SQL.php
+++ branches/will-be-merged-with-makism-bleeding-edge-upstream-dev/leaf/core/log/backend/SQL.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * This source file is licensed under the New BSD license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     http://leaf-framework.sourceforge.net/licence/  New BSD License
+ * @link        http://leaf-framework.sourceforge.net
+ */
 
 ?>
