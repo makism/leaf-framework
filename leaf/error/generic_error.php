35a0b14f6ed90975b9e59bc2cf5b29c0cabdc258
Makis Marimpis <makism@users.noreply.github.com>
2008-08-20 21:58:32 +0000

    Added.

---
+++ leaf/error/generic_error.php
@@ -0,0 +1,8 @@
+<br />
+<br />
+<div style="margin: 0px auto; width: 75%; overflow: hidden; color: #000000;">
+    <div style="font-size: 16px; background-color: #f7f7da; padding: 5px;">
+        <img src="/leaf/content/leaf/error.png" style="vertical-align: middle;"/>
+        <?php echo $message; ?>
+    </div>
+</div>
