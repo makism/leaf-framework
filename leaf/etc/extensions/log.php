3ba0e1e59891aadb55a9d741155010df1962a778
Makis Marimpis <makism@users.noreply.github.com>
2008-08-20 22:35:47 +0000

    Configuration files for the extension classes.

---
+++ leaf/etc/extensions/log.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This source file is licensed under the New BSD license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     http://leaf-framework.sourceforge.net/LICENSE  New BSD License
+ * @link        http://leaf-framework.sourceforge.net
+ *
+ * @author      Avraam Marimpis <makism@users.sourceforge.net>
+ * @version     SVN: $Id$
+ */
+
+
+global $log;
+$log = array();
+
+
+/*
+ * Logging threshold. (not implemented)
+ *
+ * Legal values: "All", "Debug", "Warning", "Info", "None"
+ * Default: "None"
+ */
+$log['log_level'] = "None";
+
+
