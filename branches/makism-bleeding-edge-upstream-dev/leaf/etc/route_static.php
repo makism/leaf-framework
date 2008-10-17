1be7cc681ecd4b7d715e7ca0672bd2227fd2c09a
makism <makism@9fb37f4e-223d-0410-90dc-a1c7b5e7249e>
2008-10-17 15:19:40 +0000

    Configuration options for Router_Static

---
+++ branches/makism-bleeding-edge-upstream-dev/leaf/etc/route_static.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This source file is licensed under the New BSD license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license     http://leaf-framework.sourceforge.net/LICENSE  New BSD License
+ * @link        http://leaf-framework.sourceforge.net
+ *
+ * @author      Avraam Marimpis <makism@users.sourceforge.net>
+ * @version     $Id$
+ */
+
+
+global $route_static;
+
+/*
+ * Default application controller.
+ */
+$route_static['default_route'] = "WelcomeApp";
+
